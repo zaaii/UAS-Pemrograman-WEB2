b251842c6347c4b601e87abe7fc7ab2bd9c1d5d9
Asriann <asriann9@gmail.com>
2022-06-02 16:40:06 +0800

    First Commit Table Master

---
+++ database/migrations/2022_06_02_070359_create_actor_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('actor', function (Blueprint $table) {
+            $table->id();
+            $table->string('actor_image');
+            $table->text('name');
+            $table->string('gender');
+            $table->date('born_date');
+            $table->integer('age');
+            $table->string('from');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('actor');
+    }
+};
